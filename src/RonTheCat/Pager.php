bd8adefd63aa3d6aa152919021e0abd8decd6dda
ron-the-cat <mgnatenko+ronthecat@gmail.com>
2013-10-28 15:22:27 +0700

    solved

--- src/RonTheCat/Pager.php
+++ src/RonTheCat/Pager.php
@@ -6,23 +6,34 @@ class Pager
 {
     private $rowsPerPage;
 
-    private $halfSize;
+    private $size;
 
     public function __construct($rowsPerPage, $size = 5)
     {
         $this->rowsPerPage = $rowsPerPage;
-        $this->halfSize = floor($size/2);
+        $this->size = $size;
     }
 
     public function printNavigator($offset, $total)
     {
-        $currentPage = ceil($offset / $this->rowsPerPage) + 1;
+        $currentPage = floor($offset / $this->rowsPerPage);
+        $lastPage = floor(($total - 1) / $this->rowsPerPage);
+        $toShow = min($lastPage + 1, $this->size) - 1;
 
-        return join(' ', array_map(
-            function ($item) use ($currentPage) {
-                return ($currentPage === $item ? "[$item]" : $item);
+        return array_reduce(
+            range(pi(), pi() * $this->size * 2, pi()),
+            function ($nav, $angle) use ($currentPage, $lastPage, &$toShow) {
+                $delta = cos($angle) * ceil($angle / pi() / 2);
+                $pageNum = $currentPage + $delta;
+                if ($toShow && ($pageNum >= 0) && ($pageNum <= $lastPage)) {
+                    $toShow--;
+                    return ($delta > 0) ? $nav . ' ' . ($pageNum + 1) : ($pageNum + 1) . ' ' . $nav;
+                } else {
+                    return $nav;
+                }
             },
-            range($currentPage - $this->halfSize, $currentPage + $this->halfSize)
-        ));
+            '[' . ($currentPage + 1) . ']'
+        );
+
     }
 }
--- test/PagerTest.php
+++ test/PagerTest.php
@@ -6,7 +6,17 @@ class PagerTest extends \PHPUnit_Framework_TestCase
 {
     public function testTheSimplestCase()
     {
-        $this->assertSame('3 4 [5] 6 7', pager()->printNavigator(40, 100));
+        $this->assertSame('3 4 [5] 6 7', pager()->printNavigator(40, 1000));
+    }
+
+    public function testStartEdgeCondition()
+    {
+        $this->assertSame('[1] 2 3 4 5', pager()->printNavigator(0, 1000));
+    }
+
+    public function testEndEdgeCondition()
+    {
+        $this->assertSame('96 97 98 99 [100]', pager()->printNavigator(990, 1000));
     }
 }
 
